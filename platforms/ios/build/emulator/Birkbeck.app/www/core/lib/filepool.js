d77d010bec6858373330dfcda51c701db44125b0
Paul Sharpe <paul@slapp.dcs.bbk.ac.uk>
2016-04-26 15:06:35 +0100

    - iOS: No landscape orientation.

--- platforms/ios/build/emulator/Birkbeck.app/www/core/lib/filepool.js
+++ platforms/ios/build/emulator/Birkbeck.app/www/core/lib/filepool.js
@@ -651,7 +651,6 @@ angular.module('mm.core')
                 return self.storePackageStatus(siteId, component, componentId, mmCoreDownloaded, revision, timemod);
             }).catch(function() {
                 // Error downloading, go back to previous status and reject the promise.
-		$log.debug('Error downloading package.');
                 return self.setPackagePreviousStatus(siteId, component, componentId).then(function() {
                     return $q.reject();
                 });
